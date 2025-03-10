121cac7de72021024a7e38dff934c2fad71125ba
Souravmanjee <souravkumarvicky42@gmail.com>
2025-03-11 01:41:02 +0530

    fetching data

--- server/routes/addconnection.js
+++ server/routes/addconnection.js
@@ -1,4 +1,5 @@
 const connect=require ('../models/connections')
+const station=require('../models/stations')
 const server = require ('express')
 const app=server()
 
@@ -8,9 +9,44 @@ async function apicall(req,res){
     try {
         const connection=new connect(data);
         await connection.save();
+        await createmap();
+        // await connectionliist();
         res.json("data saved successfully");
     } catch (error) {
         res.json(error);
     }
 }
+async function createmap(){
+    let tation;
+    try {
+        tation = await station.find();
+        const n=tation.length;
+        //    const n = 4;
+           const matrix = [];
+           for (let i = 0; i < n; i++) {
+               matrix.push(new Array(n).fill(0));
+           }
+           console.log(matrix);
+    } catch(error){
+        console.log(error);
+    }
+
+
+    try {
+        const liist=await connect.find();
+        // await liist.save();
+        // console.log(liist);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    const stationname= []
+    for(let i=0;i<tation.length;i++){
+        // console.log(tation[i]);  
+        stationname.push(tation[i].stations);
+    }
+    console.log(stationname);
+
+}                       
+
 module.exports=app;
