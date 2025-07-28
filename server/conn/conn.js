8103e8593c9d7080220f754a937d3044c29c812f
Souravmanjee <souravkumarvicky42@gmail.com>
2025-07-28 17:33:53 +0530

    env hide

--- server/conn/conn.js
+++ server/conn/conn.js
@@ -1,10 +1,28 @@
-const db=require ('mongoose')
-const connection = "mongodb+srv://hunterbaaba:Sourhub@metroguide.rj7cz.mongodb.net/?retryWrites=true&w=majority&appName=Metroguide"
+// const db=require ('mongoose')
+// const connection = "mongodb+srv://hunterbaaba:Sourhub@metroguide.rj7cz.mongodb.net/?retryWrites=true&w=majority&appName=Metroguide"
 
-db.connect(connection,{}).then(callbck).catch(errorfun);
-function errorfun(error){
-    console.log(error);
-}
-function callbck(){
-    console.log("mongoDB connected")
-}
+// db.connect(connection,{}).then(callbck).catch(errorfun);
+// function errorfun(error){
+//     console.log(error);
+// }
+// function callbck(){
+//     console.log("mongoDB connected")
+// }
+
+require('dotenv').config(); // Load environment variables from .env
+const mongoose = require('mongoose');
+
+// Get Mongo URI from environment
+const connection = process.env.MONGO_URI;
+
+// Connect to MongoDB
+mongoose.connect(connection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("✅ MongoDB connected");
+})
+.catch((error) => {
+  console.error("❌ MongoDB connection error:", error);
+});
