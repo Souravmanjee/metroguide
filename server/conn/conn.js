23e28d5d82ae1ceb0099a8a8304f3b5182de86d0
Souravmanjee <souravkumarvicky42@gmail.com>
2025-07-28 17:35:15 +0530

    env hide

--- server/conn/conn.js
+++ server/conn/conn.js
@@ -1,14 +1,3 @@
-// const db=require ('mongoose')
-// const connection = "mongodb+srv://hunterbaaba:Sourhub@metroguide.rj7cz.mongodb.net/?retryWrites=true&w=majority&appName=Metroguide"
-
-// db.connect(connection,{}).then(callbck).catch(errorfun);
-// function errorfun(error){
-//     console.log(error);
-// }
-// function callbck(){
-//     console.log("mongoDB connected")
-// }
-
 require('dotenv').config(); // Load environment variables from .env
 const mongoose = require('mongoose');
 
